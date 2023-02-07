702bf570d6c5e27b4cb96cfa8cf91b4115a2f21f
Michael Heerkens <michael.heerkens@michaels-mbp-2.home>
2023-02-07 22:55:11 +0100

    new files

--- main.js
+++ main.js
@@ -1,67 +1,50 @@
-import { Actor } from 'apify';
-import { PlaywrightCrawler } from 'crawlee';
+import { test, expect } from '@playwright/test';
+//const fs = require('fs');
 
-await Actor.init();
+test('test', async ({ page }) => {
+  await page.goto('https://mypadelclubkrimpen.baanreserveren.nl/');
+  await page.locator('input[name="username"]').click();
+  await page.locator('input[name="username"]').fill('Michael84');
+  await page.locator('input[name="password"]').click();
+  await page.locator('input[name="password"]').fill('&nFjY61t&NBgu4');
+  await page.getByRole('button', { name: 'Inloggen' }).click();
 
-// Create an instance of the PlaywrightCrawler class - a crawler
-// that automatically loads the URLs in headless Chrome / Playwright.
-const crawler = new PlaywrightCrawler({
-    launchContext: {
-        // Here you can set options that are passed to the playwright .launch() function.
-        launchOptions: {
-            headless: true,
-        },
-    },
+  const findTime = '21:00';
+  const findDate = '2023-2-8';
 
-    // Stop crawling after several pages
-    maxRequestsPerCrawl: 50,
+  await page.goto('https://mypadelclubkrimpen.baanreserveren.nl/reservations/' + findDate + '/sport/1251');
 
-    // This function will be called for each URL to crawl.
-    // Here you can write the Playwright scripts you are familiar with,
-    // with the exception that browsers and pages are automatically managed by the Apify SDK.
-    // The function accepts a single parameter, which is an object with a lot of properties,
-    // the most important being:
-    // - request: an instance of the Request class with information such as URL and HTTP method
-    // - page: Playwright's Page object (see https://playwright.dev/docs/api/class-page)
-    async requestHandler({ request, page, enqueueLinks }) {
-        console.log(`Processing ${request.url}...`);
+  const rowPath = '#tbl_matrix > tbody > tr > td > table > tbody > tr[data-time="' + findTime + '"]';
+  const findRow = await page.locator(rowPath);
+  const findUTC = await findRow.getAttribute('utc');
 
-        // A function to be evaluated by Playwright within the browser context.
-        const data = await page.$$eval('.athing', ($posts) => {
-            const scrapedData = [];
+  let findSlot = '0';
 
-            // We're getting the title, rank and URL of each post on Hacker News.
-            $posts.forEach(($post) => {
-                scrapedData.push({
-                    title: $post.querySelector('.title a').innerText,
-                    rank: $post.querySelector('.rank').innerText,
-                    href: $post.querySelector('.title a').href,
-                });
-            });
+  for (const row of await findRow.getByRole('cell').all()) {
+    const type = await row.getAttribute('type');
+    if(type == 'free') {
+      const tmpSlot = await row.getAttribute('slot');
+      findSlot =  tmpSlot ?? '';
+      break;
+    }
+  }
 
-            return scrapedData;
-        });
+  const reservationUrl = 'https://mypadelclubkrimpen.baanreserveren.nl/reservations/make/' + findSlot + '/' + findUTC;
 
-        // Store the results to the default dataset.
-        await Actor.pushData(data);
+  console.log(reservationUrl);
 
-        // Find a link to the next page and enqueue it if it exists.
-        const infos = await enqueueLinks({
-            selector: '.morelink',
-        });
+  await page.goto(reservationUrl);
 
-        if (infos.processedRequests.length === 0) console.log(`${request.url} is the last page!`);
-    },
+  await page.locator('select[name="players\\[2\\]"]').selectOption('-1');
+  await page.locator('select[name="players\\[3\\]"]').selectOption('-1');
+  await page.locator('select[name="players\\[4\\]"]').selectOption('-1');
+  await page.getByText('Verder').click();
 
-    // This function is called if the page processing failed more than maxRequestRetries+1 times.
-    failedRequestHandler({ request }) {
-        console.log(`Request ${request.url} failed too many times.`);
-    },
+  const hasBtn = await expect(page.getByText('Bevestigen')).toBeVisible();
+  console.log(hasBtn);
+
+  // Save the screenshot to a file
+  //const screenshot = await page.screenshot();
+  //fs.writeFileSync('screenshot.png', screenshot);
+
 });
-
-// Run the crawler and wait for it to finish.
-await crawler.run(['https://news.ycombinator.com/']);
-
-console.log('Crawler finished.');
-
-await Actor.exit();
